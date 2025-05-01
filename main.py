486c58e4111816f9243374de61d9c46f90120b18
高乐喆 <gaolezhe@outlook.com>
2025-05-01 21:14:09 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import getpass
 import os
 import shutil
 
-
 def error():
     print(ERROR + " ------------------------------------------------")
     print(ERROR + " + 按任意键退出FixGreenHub")
@@ -30,35 +29,34 @@ def fix(path):
         error()
 
 
-def check_path():
-    path = getpath()
-    status = os.path.exists(path)
-    if status:
-        print(INFO + "获取GreenHub路径成功")
-        print(INFO + " ------------------------------------------------")
-        print(INFO + " + 路径: "+path)
-        print(INFO + " ------------------------------------------------")
-        fix(path)
-    else:
-        print(ERROR + "获取GreenHub路径错误！")
-        print(ERROR + " ------------------------------------------------")
-        print(ERROR + " + 请检查GreenHub是否安装在默认路径")
-        print(ERROR + " + 暂不支持自定义路径")
-        print(ERROR + " + 若非以上问题，请联系：2049621985@qq.com")
-        print(ERROR + " ------------------------------------------------")
-        error()
-
-
 def getpath():
     user = getpass.getuser()
     path = f'C:\\Users\\{user}\\AppData\\Roaming\\GreenHub'
     return path
 
+def check_path():
+    path=getpath()
+    status = os.path.exists(path)
+    if status:
+        print(INFO + "获取GreenHub数据储存路径（非安装路径）成功")
+        print(INFO + " ------------------------------------------------")
+        print(INFO + " + 路径: "+path)
+        print(INFO + " ------------------------------------------------")
+        fix(path)
+    else:
+        print(ERROR + "获取GreenHub数据储存路径（非安装路径）错误！")
+        print(ERROR + " ------------------------------------------------")
+        error()
+
 
 if __name__ == '__main__':
-    Introduce = "___________          _________                       ______  __      ______\n___  ____/__(_)___  ___  ____/__________________________  / / /___  ____  /_ \n__  /_   _/  /__  |/_/  / __ __  ___/  _ \  _ \_  __ \_  /_/ /_  / / /_  __ \ \n_  __/   _/ / __>  < / /_/ / _  /   /  __/  __/  / / /  __  / / /_/ /_  /_/ / \n/_/      /_/  /_/|_| \____/  /_/    \___/\___//_/ /_//_/ /_/  \__,_/ /_.___/ \n + ©2023 WLHYW. All rights reserved. \n + Version beta 1.0.0 \n"
+    attempt=False
     INFO = "[\033[92mINFO\033[0m]"
     ERROR = "[\033[91mERROR\033[0m]"
-    print(Introduce)
+    try:
+        os.system("taskkill /im Greenhub.exe /f")
+    except:
+        pass
     check_path()
     input()
+
